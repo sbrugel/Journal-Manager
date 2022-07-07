629bad3393f514bc7354ec513282834f0e5d85c2
sbrugel <sbrugel62@gmail.com>
2022-07-07 10:17:16 -0400

    tags now saved with entry

--- Journal Manager/EntryCreator.cs
+++ Journal Manager/EntryCreator.cs
@@ -54,6 +54,9 @@ namespace Journal_Manager
                 contentBox.ReadOnly = true;
                 titleBox.ReadOnly = true;
                 colorChoice.Enabled = false;
+                tagsList.Visible = false;
+                addTag.Visible = false;
+                label2.Visible = false;
 
                 titleLabel.Text = "Journal Title";
                 contentLabel.Text = "Content";
@@ -93,6 +96,12 @@ namespace Journal_Manager
                 File.WriteAllText(saveAs, contentBox.Text);
                 File.AppendAllText(saveAs, "<TITLE>" + titleBox.Text + "</TITLE>");
                 File.AppendAllText(saveAs, "<COLOR>" + color + "</COLOR>");
+                List<string> tagNames = new List<string>();
+                foreach (ListViewItem tag in currentTags.Items)
+                {
+                    tagNames.Add(tag.Text);
+                }
+                File.AppendAllText(saveAs, "<TAGS>" + String.Join(",", tagNames) + "</TAGS>");
                 MessageBox.Show("Saved as " + saveAs, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             catch (Exception ex)
@@ -108,11 +117,42 @@ namespace Journal_Manager
                 string contents = SubstringFromTo(rawText, 0, rawText.IndexOf("<TITLE>"));
                 string title = SubstringFromTo(rawText, rawText.IndexOf("<TITLE>") + 7, rawText.IndexOf("</TITLE>"));
                 color = SubstringFromTo(rawText, rawText.IndexOf("<COLOR>") + 7, rawText.IndexOf("</COLOR>"));
+                string tags = SubstringFromTo(rawText, rawText.IndexOf("<TAGS>") + 6, rawText.IndexOf("</TAGS>")); // if no tags, will show None
 
                 savedText = contents;
                 contentBox.Text = contents;
                 titleBox.Text = title.Equals("None") ? "" : title;
                 GetColor();
+
+                if (!tags.Equals("None"))
+                {
+                    int addIteration = 0;
+                    string[] tagsList = tags.Split(',');
+                    foreach (string tag in tagsList)
+                    {
+                        // search through saved tags and find the right one (for color)
+                        string tagsDirectory = File.ReadLines(DATA_FILE).ElementAtOrDefault(0) + "\\tags"; // first line
+
+                        string[] savedTags = Directory.GetFiles(tagsDirectory);
+
+                        foreach (string savedTag in savedTags)
+                        {
+                            string tagRaw = File.ReadAllText(savedTag);
+                            string tagName = SubstringFromTo(tagRaw, 0, tagRaw.IndexOf("<COLOR>"));
+                            if (!Path.GetExtension(savedTag).Equals(".tag") || !tagName.Equals(tag)) continue;
+                            string tagColor = SubstringFromTo(tagRaw, tagRaw.IndexOf("<COLOR>") + 7, tagRaw.IndexOf("</COLOR>"));
+                            string red = SubstringFromTo(tagColor, 0, indexOfNth(tagColor, "/", 0));
+                            string green = SubstringFromTo(tagColor, indexOfNth(tagColor, "/", 0) + 1, indexOfNth(tagColor, "/", 1));
+                            string blue = SubstringFromTo(tagColor, indexOfNth(tagColor, "/", 1) + 1, tagColor.Length);
+
+                            currentTags.Items.Add(tagName);
+                            currentTags.Items[addIteration].BackColor = Color.FromArgb(Int32.Parse(red), Int32.Parse(green), Int32.Parse(blue));
+                            addIteration++;
+
+                            break;
+                        }
+                    }
+                }
 
                 if (readOnly) // format text only if not editing
                 {
@@ -377,6 +417,7 @@ namespace Journal_Manager
         private void ToggleButton()
         {
             addTag.Enabled = true;
+            if (tagsList.SelectedItem == null) return;
             foreach (ListViewItem tag in currentTags.Items)
             {
                 if (tag.Text.Equals(tagsList.SelectedItem.ToString()))
@@ -408,6 +449,7 @@ namespace Journal_Manager
 
         private void tagDoubleClick(object sender, EventArgs e)
         {
+            if (readOnly) return;
             currentTags.Items.Remove(currentTags.SelectedItems[0]);
             ToggleButton();
         }
